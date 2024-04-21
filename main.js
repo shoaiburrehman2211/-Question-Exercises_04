973cf336db1e8d8cff11b9e71a223102f107908a
Shoaib-ur-Rehman <160622745+shoaiburrehman2211@users.noreply.github.com>
2024-04-22 02:16:04 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+/*
+Find a quote from a famous person you admire. Print the quote and the name of its author.
+Your output should look something like the following, including the quotation marks:
+Albert Einstein once said, “A person who never made a mistake never tried anything new.
+*/
+var myQuote = " Albert Einstein once said, \u201CA person who never\n made a mistake never tried anything new.\" ";
+//backtick
+console.log(myQuote);
